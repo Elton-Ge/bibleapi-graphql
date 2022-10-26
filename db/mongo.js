a1a2e25f7fb8d22fa8f0024147ae3329463aebba
ELTON-GE <gexin7648@hotmail.com>
2022-10-26 12:58:06 +1100

    update api

--- db/mongo.js
+++ db/mongo.js
@@ -1,23 +1,29 @@
 module.exports = mongoPool => {
-  return {
-    getVerses(query) {
-      return mongoPool.collection(process.env.MONGODB_COLLECTION)
-        .find(query)
-        .toArray()
-        .then(rows =>
-          rows.map((row) => ({
-            id: row._id,
-            book: row.book_id,
-            chapter: row.chapter,
-            verse: row.verse,
-            text: row.text
-          })));
-    },
-    getText(query) {
-      return mongoPool.collection(process.env.MONGODB_COLLECTION)
-        .find(query)
-        .toArray()
-        .then(rows => rows.map(row => row.text).join(' '));
-    },
-  }
+    return {
+        getVerses(query) {
+            return mongoPool.collection(process.env.MONGODB_COLLECTION)
+                .find(query)
+                .toArray()
+                .then(rows => {
+                        return rows.sort((a, b) =>
+                            a.verse - b.verse
+                        ).map((row) => ({
+                            id: row._id,
+                            book: row.bookName,
+                            chapter: row.chapter,
+                            verse: row.verse,
+                            text: row.text
+                        }))
+                    }
+                );
+        },
+        getText(query) {
+            return mongoPool.collection(process.env.MONGODB_COLLECTION)
+                .find(query)
+                .toArray()
+                .then(rows => rows.sort((a, b) =>
+                    a.verse - b.verse
+                ).map(row => row.text).join(' '));
+        },
+    }
 };
